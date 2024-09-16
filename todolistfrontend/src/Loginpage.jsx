595785bdd163515792d5bd5df22e766d9fce9718
Shubham <shubham.sms142@gmail.com>
2024-09-16 22:58:30 +0530

    minor space changes

--- todolistfrontend/src/Loginpage.jsx
+++ todolistfrontend/src/Loginpage.jsx
@@ -85,7 +85,6 @@ const Loginpage = () => {
           </button>
         </div>
       </form>
-
       <p className="mt-10 text-center text-sm text-gray-500">
         Not a member?{' '}
         <a href="#" className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">
